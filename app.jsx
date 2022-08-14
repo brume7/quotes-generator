b5f45fa726574bc0905061626ce6c19e7f72fa8c
brume7 <brumeakpadaka@gmail.com>
2022-08-14 22:57:41 +0100

    responsive issue fixed

--- app.jsx
+++ app.jsx
@@ -37,8 +37,8 @@ class QuoteDiv extends React.Component{
             <div className="card-body" id="quote-box">
                 
                 <div className="card-body" >
-                    <h3 className="card-title" id="text" >&ldquo;{this.state.quote}&rdquo; &mdash; </h3>
-                    <h6 className="card-subtitle mb-2 text-muted" id="author">{this.state.author}</h6>
+                    <h3 className="card-title" id="text" >&ldquo;{this.state.quote}&rdquo;</h3>
+                    <h6 className="card-subtitle mb-2 text-muted" id="author"> &mdash; {this.state.author}</h6>
                     <div className="row">
                     <a id="tweet-quote" target="_blank" href={tweet_url} className="btn btn-primary"><i className="fa fa-twitter"></i></a>
                     <button id="new-quote" className="btn btn-primary right" onClick={()=>{this.getJSONData()}}>New quote</button>
